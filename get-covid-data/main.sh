bf7282584c55a1f37d7436e550aba5db56c4bde1
Blair Drummond <blairdrummond@protonmail.com>
2020-04-22 17:16:00 -0400

    Fetch at runtime

--- get-covid-data/main.sh
+++ get-covid-data/main.sh
@@ -1,23 +1,25 @@
-
 #!/usr/bin/env sh
 
-# Compute pi.
-#
 # Input:
 #     --output  Output bucket/folder
 #     --params  Json containing integer "seed"
 #
-# Output:
-#     in.json and out.json
-#
-#     out.json has as "result" field, which is 0 or 1, depending
-#     on whether the random point is inside or outside the circle.
-#
-# Blair Drummond
-#
+
+# Get the source
+git clone --depth=1 \
+    https://github.com/kennethchu-statcan/covid19/ \
+    /tmp/covid19 || exit 1
+mv /tmp/covid19/005-test-kubeflow/pipeline-test/image-loadData/src /src
+
+
 
 while test -n "$1"; do
     case "$1" in
+        --data)
+            shift
+            DATA="$1"
+            ;;
+
         --output)
             shift
             OUTPUT="$1"
@@ -35,6 +37,17 @@ while test -n "$1"; do
     esac
     shift
 done
+
+
+#########################
+###  Fetch the Data  ####
+#########################
+echo "MINIO: -> _${S3_ENDPOINT}_"
+mc config host add daaas \
+    "http://$S3_ENDPOINT" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
+
+echo mc cp -r "daaas/$DATA" /data
+mc cp -r "daaas/$DATA" /data
 
 
 echo "Input: "
@@ -58,10 +71,6 @@ R --no-save --args ${dataDIR} ${codeDIR} ${outputDIR} < ${myRscript} > ${stdoutF
 #############
 #############
 
-
-echo "MINIO: -> _${S3_ENDPOINT}_"
-mc config host add daaas \
-    "http://$S3_ENDPOINT" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
 
 echo mc cp -r /output "daaas/$OUTPUT"
 mc cp -r /output "daaas/$OUTPUT"
