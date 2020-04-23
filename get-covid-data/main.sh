e32bba47fc7c9b8370384eab58a88e4f2c0c634b
Blair Drummond <blairdrummond@protonmail.com>
2020-04-22 20:23:23 -0400

    Tidying up

--- get-covid-data/main.sh
+++ get-covid-data/main.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env sh
 
 # Input:
+#     --data    Input  bucket/folder
 #     --output  Output bucket/folder
-#     --params  Json containing integer "seed"
+#     --params  Json parameters
 #
 
 # Get the source
-git clone --depth=1 \
+if ! git clone --depth=1 \
     https://github.com/kennethchu-statcan/covid19/ \
-    /tmp/covid19 || exit 1
-mv /tmp/covid19/005-test-kubeflow/pipeline-test/image-loadData/src /src
-
+    /tmp/covid19; then
+    echo "Couldn't git clone. Exiting. Network Error?"
+    exit 1
+fi
 
 
 while test -n "$1"; do
@@ -31,7 +33,7 @@ while test -n "$1"; do
             ;;
 
         *)
-            echo "Invalid option $1; allowed: --params --options" >&2
+            echo "Invalid option $1; allowed: --data --params --options" >&2
             exit 1
             ;;
     esac
@@ -50,6 +52,7 @@ echo mc cp -r "daaas/$DATA" /data
 mc cp -r "daaas/$DATA" /data
 
 
+mkdir -p /output
 echo "Input: "
 echo "$JSON" | jq '.' | tee /output/in.json
 
@@ -57,7 +60,6 @@ echo "$JSON" | jq '.' | tee /output/in.json
 # This is where the single-run modelling R command can go ... ?
 #############
 
-mkdir -p /output
 
   dataDIR=/data
   codeDIR=/src
@@ -67,6 +69,8 @@ outputDIR=/output
 stdoutFile=${outputDIR}/stdout.R.`basename ${myRscript} .R`
 stderrFile=${outputDIR}/stderr.R.`basename ${myRscript} .R`
 R --no-save --args ${dataDIR} ${codeDIR} ${outputDIR} < ${myRscript} > ${stdoutFile} 2> ${stderrFile}
+
+echo 'R finished.'
 
 #############
 #############
