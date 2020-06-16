60b451331b9ddd42f8ae328ef8c30a49dc46ffb8
chritter <christian.ritter@canada.ca>
2020-06-16 09:10:23 -0400

    package install resolved

--- constrstarts-geo/install_requirements.sh
+++ constrstarts-geo/install_requirements.sh
@@ -11,7 +11,7 @@
 
 . activate base && pip install -r requirements.txt
 #. activate base && conda install -c conda-forge rtree==0.9.4 gdal==3.1.0
-# . activate base && pip install solaris==0.2.0 --ignore-installed PyYAML torchvision==0.5.0
+# !! install then: pip install solaris==0.2.0 --ignore-installed PyYAML torchvision==0.5.0
 
 # due to ignore-installed, install packages separately
 #. activate fastai && pip install solaris==0.2.0 --ignore-installed PyYAML torchvision==0.5.0
