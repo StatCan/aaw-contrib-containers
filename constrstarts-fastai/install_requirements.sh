44ce4f3889d57939752b781ec4c6b3c9f6113406
Christian Ritter <christian.ritter@canada.ca>
2020-06-19 15:30:33 -0400

    Update install_requirements.sh

--- constrstarts-fastai/install_requirements.sh
+++ constrstarts-fastai/install_requirements.sh
@@ -1,11 +1,27 @@
-# python package installation script
+#!/usr/bin/env bash
 
+# Python packages.
+apt-get update
+apt-get install -y software-properties-common --no-install-recommends
+apt-get clean
+rm -rf /var/lib/apt/lists/* 
 
-conda update -n base -c defaults conda
-. activate fastai && conda update -y fastai
-. activate fastai && pip install -r requirements.txt
-. activate fastai && conda install -c conda-forge rtree==0.9.4 gdal==3.1.0
-. activate fastai && pip install solaris==0.2.0 --ignore-installed PyYAML torchvision==0.5.0
+# Updating conda.
+conda update -n base -c defaults -y conda
 
-# due to ignore-installed, install packages separately
-#. activate fastai && pip install solaris==0.2.0 --ignore-installed PyYAML torchvision==0.5.0
+# Activating fastai conda env and installing the required packages.
+. activate fastai
+conda update -y --all
+pip install -r requirements.txt
+conda install -c conda-forge rtree==0.9.4 gdal==3.1.0
+pip install solaris==0.2.0 --ignore-installed PyYAML torchvision==0.5.0
+
+# Removing unnecessary packages.
+apt autoremove
+
+# Removal due to security risk.
+apt-get remove -y libxml2
+rm -rf /notebooks
+
+# Cloning a reference Git repo.
+git clone --depth=1 https://github.com/daveluo/zanzibar-aerial-mapping.git
