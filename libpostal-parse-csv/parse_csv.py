4b33917fc9c1b541ebd09a36a32489e69abe98e0
Joseph Kuchar <joseph.kuchar@canada.ca>
2020-12-15 09:33:58 -0500

    Update parse_csv.py

--- libpostal-parse-csv/parse_csv.py
+++ libpostal-parse-csv/parse_csv.py
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     name_in = args.name_in
     addr_col = args.addr_col
     name_out = args.name_out
-    df_in = pd.read_csv(name_in)
+    df_in = pd.read_csv(name_in, dtype='str')
+    df_in=df_in.fillna('')
     df_out = parse_csv(df_in, addr_col)
     df_out.to_csv(name_out, index=False)
