992a0318d61b12eb1b3b2361ac43e0d297572fea
Bukola Ishola <maybuksho@yahoo.com>
2021-11-22 15:04:54 +0000

    updating src

--- btap/src/feature_selection.py
+++ btap/src/feature_selection.py
@@ -1,6 +1,5 @@
 '''
-Select features that are used to build the surrogate mode. 
-
+Select features that are used to build the surrogate mode.
 '''
 from sklearn.feature_selection import RFECV 
 from sklearn.linear_model import LinearRegression, LassoCV, Lasso,ElasticNetCV
@@ -12,31 +11,32 @@ import numpy as np
 import xgboost as xgb 
 import json
 import argparse
+import json
+import config as acm
+import numpy as np
 import pandas as pd
 import s3fs
 
-############################################################    
+############################################################
 # feature selection
 ############################################################
 
-def select_features(args,estimator_type ='lasso', min_features=20): 
+
+def select_features(args, estimator_type='lasso', min_features=20):
     """
     Select the feature which contribute most to the prediction for the total energy consumed.
-    
+
     Default estimator_type used for feature selection is 'LassoCV'
-    
+
     Args:
         args: arguements provided from the main
 
     Returns:
-       selected features are returned and uploaded to minio. 
-       
+       selected features are returned and uploaded to minio.
     """
-    data = acm.access_minio(tenant=args.tenant,
-                           bucket=args.bucket,
-                           operation= 'read',
-                           path=args.in_obj_name,
-                           data='')
+    data = acm.access_minio(operation='read',
+                            path=args.in_obj_name,
+                            data='')
 
     data = json.load(data)
     features =data["features"] 
@@ -48,29 +48,27 @@ def select_features(args,estimator_type ='lasso', min_features=20):
     scalery= RobustScaler()
     X_train = scalerx.fit_transform(data["X_train"])
     y_train = pd.read_json(data["y_train"], orient='values').values.ravel()
-    
+
     if estimator_type == "linear":
         estimator = LinearRegression()
-        rfecv = RFECV(estimator=estimator, step=1, cv=KFold(10),scoring='neg_mean_squared_error',min_features_to_select=min_features)
-        fit = rfecv.fit(X_train,y_train)
-        rank_features_nun = pd.DataFrame(rfecv.ranking_, columns=["rank"], index = data["features"])
-        selected_features = rank_features_nun.loc[rank_features_nun["rank"]==1].index.tolist() 
-    
+        rfecv = RFECV(estimator=estimator, step=1, cv=KFold(10), scoring='neg_mean_squared_error', min_features_to_select=min_features)
+        fit = rfecv.fit(X_train, y_train)
+        rank_features_nun = pd.DataFrame(rfecv.ranking_, columns=["rank"], index=data["features"])
+        selected_features = rank_features_nun.loc[rank_features_nun["rank"] == 1].index.tolist()
     elif estimator_type == "elasticnet":
-        reg = ElasticNetCV(n_jobs=-1,cv=10)
-        fit = reg.fit(X_train,y_train)
-        rank_features_nun = pd.DataFrame(rfecv.ranking_, columns=["rank"], index = data["features"])
-        selected_features = rank_features_nun.loc[rank_features_nun["rank"]==1].index.tolist()
-        score = rfecv.score(X_train,y_train)
-        rank_features_nun = pd.DataFrame(reg.coef_, columns=["rank"], index = data["features"])
-        selected_features = rank_features_nun.loc[abs(rank_features_nun["rank"])>0].index.tolist()
-        
+        reg = ElasticNetCV(n_jobs=-1, cv=10)
+        fit = reg.fit(X_train, y_train)
+        rank_features_nun = pd.DataFrame(rfecv.ranking_, columns=["rank"], index=data["features"])
+        selected_features = rank_features_nun.loc[rank_features_nun["rank"] == 1].index.tolist()
+        score = rfecv.score(X_train, y_train)
+        rank_features_nun = pd.DataFrame(reg.coef_, columns=["rank"], index=data["features"])
+        selected_features = rank_features_nun.loc[abs(rank_features_nun["rank"]) > 0].index.tolist()
     elif estimator_type == "xgb":
-        estimator = xgb.XGBRegressor(n_jobs = -1)
-        rfecv = RFECV(estimator=estimator, step=1, cv=KFold(10),scoring='neg_mean_squared_error', min_features_to_select=min_features)
-        fit = rfecv.fit(X_train,y_train)
-        rank_features_nun = pd.DataFrame(rfecv.ranking_, columns=["rank"], index = data["features"])
-        selected_features = rank_features_nun.loc[rank_features_nun["rank"]==1].index.tolist()
+        estimator = xgb.XGBRegressor(n_jobs=-1)
+        rfecv = RFECV(estimator=estimator, step=1, cv=KFold(10), scoring='neg_mean_squared_error', min_features_to_select=min_features)
+        fit = rfecv.fit(X_train, y_train)
+        rank_features_nun = pd.DataFrame(rfecv.ranking_, columns=["rank"], index=data["features"])
+        selected_features = rank_features_nun.loc[rank_features_nun["rank"] == 1].index.tolist()
     else:
         reg = linear_model.LassoCV(cv=10,n_jobs=-1,n_alphas=100,tol=600)
         fit = reg.fit(X_train,y_train)
@@ -81,32 +79,25 @@ def select_features(args,estimator_type ='lasso', min_features=20):
         print(len(selected_features))
         print(selected_features)
 
-    data = {'features' : selected_features}
+    data = {'features': selected_features}
     data_json = json.dumps(data).encode('utf-8')
-    
-    #copy data to minio
-    acm.access_minio(tenant=args.tenant,
-                 bucket=args.bucket,
-                 operation='copy',
-                 path=args.output_path,
-                 data=data_json)
-    
-    return              
-        
+
+    # copy data to minio
+    acm.access_minio(operation='copy',
+                     path=args.output_path,
+                     data=data_json)
+
+    return
+
 
 if __name__ == '__main__':
-    
     parser = argparse.ArgumentParser()
-    
+
     # Paths must be passed in, not hardcoded
-    parser.add_argument('--tenant', type=str, help='The minio tenant where the data is located in')
-    parser.add_argument('--bucket', type=str, help='The minio bucket where the data is located in')
     parser.add_argument('--in_obj_name', type=str, help='Name of data file to be read')
     parser.add_argument('--estimator_type', type=str, help='Name of data file to be read')
     parser.add_argument('--output_path', type=str, help='Path of the local file where the output file should be written.')
     args = parser.parse_args()
-    
-    select_features(args)
-    #python3 feature_selection.py --tenant standard --bucket nrcan-btap --in_obj_name output_data/preprocessing_out --output_path output_data/feature_out --estimator_type elasticnet
 
-    
+    select_features(args)
+    # python3 feature_selection.py --in_obj_name output_data/preprocessing_out --output_path output_data/feature_out --estimator_type lasso
