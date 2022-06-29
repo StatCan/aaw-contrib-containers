0396db81f4f88c13c2ccea436968affd61edb9d6
rohank07 <rohank_17@hotmail.ca>
2022-06-29 11:00:56 -0400

    remove scanning

--- hive-metastore/entrypoint.sh
+++ hive-metastore/entrypoint.sh
@@ -3,5 +3,5 @@ export HADOOP_HOME=/opt/hadoop-3.3.3
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
 export JAVA_HOME=/usr/java/openjdk-11
 
-/opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType postgres
-/opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
+/opt/apache-hive-metastore-3.1.1-bin/bin/schematool -initSchema -dbType postgres
+/opt/apache-hive-metastore-3.1.1-bin/bin/start-metastore
