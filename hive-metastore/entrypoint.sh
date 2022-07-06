6603e19d0ea5ceded33d5cf1dbe67ca416e4ac79
vexingly <pat.ledgerwood@statcan.gc.ca>
2022-07-06 15:56:15 -0400

    fix(hive): patch CVE in dockerfile and update schematool logic

--- hive-metastore/entrypoint.sh
+++ hive-metastore/entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-export HADOOP_HOME=/opt/hadoop-3.3.3
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
+export PATH=${HIVE_HOME}/bin:${HADOOP_HOME}/bin:$PATH
 export JAVA_HOME=/usr/java/openjdk-11
 
-/opt/apache-hive-metastore-3.1.3-bin/bin/schematool -dbType postgres -upgradeSchema -verbose
-/opt/apache-hive-metastore-3.1.3-bin/bin/start-metastore
+# Check if schema exists / print schema version
+if schematool -dbType postgres -info -verbose; then
+
+    # If schema exists validate and upgrade
+    schematool -dbType postgres -validate -verbose
+    schematool -dbType postgres -upgradeSchema -verbose
+else
+
+    # If schema does not exist, create it
+    if schematool -dbType postgres -initSchema -verbose; then
+        echo "Hive metastore schema created."
+    else
+        echo "Error creating hive metastore: $?"
+    fi
+fi
+
+start-metastore
